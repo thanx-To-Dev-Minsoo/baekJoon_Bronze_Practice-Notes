de24a1e06db499de44f783f2eb8ebb68991a2a2b
MinSoo <136697128+thanx-To-Dev-Minsoo@users.noreply.github.com>
2024-07-09 15:53:53 +0900

    2024-07-09

---
+++ src/bronze_1008.java
@@ -0,0 +1,17 @@
+//A/B - 1008
+//두 정수 A와 b를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+
+import java.util.Scanner;
+
+public class bronze_1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a / b);
+
+        sc.close();
+    }
+}
