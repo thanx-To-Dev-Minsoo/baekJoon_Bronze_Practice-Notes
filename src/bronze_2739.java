8f1144e043b9d1130aede15ee3e39bef86c70602
MinSoo <136697128+thanx-To-Dev-Minsoo@users.noreply.github.com>
2024-07-11 14:25:57 +0900

    2024-07-11

--- src/bronze_2739.java
+++ src/bronze_2739.java
@@ -1,7 +1,23 @@
 //구구단 - 2739
 //N을 입력받은 뒤, 구구단 N단을 출력하는 프로그램을 작성하시오. 출력 형식에 맞춰서 출력하면 된다.
 
-import java.util.Scanner;
+//import java.util.Scanner;
+//
+//public class bronze_2739 {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//
+//        int N = sc.nextInt();
+//
+//        for (int i = 0; i <= 9; i ++) {
+//            System.out.println(N + "*" + i + "=" + (N * i));
+//        }
+//
+//        sc.close();
+//    }
+//}
+
+import java.util.*;
 
 public class bronze_2739 {
     public static void main(String[] args) {
@@ -9,8 +25,8 @@ public class bronze_2739 {
 
         int N = sc.nextInt();
 
-        for (int i = 0; i <= 9; i ++) {
-            System.out.println(N + "*" + i + "=" + (N * i));
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(N + " * " + i + " = " + (N * i));
         }
 
         sc.close();
